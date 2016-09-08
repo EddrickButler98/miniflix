2ca404a2392909ef5900452666e10316194d49ad
Eddrick Butler <ebutler@basecampcodingacademy.org>
2016-09-08 09:25:08 -0500

    Added movie urls

--- js/miniflix.js
+++ js/miniflix.js
@@ -1,2 +1,16 @@
 // Add video links to this list 
-var video_urls = [];
+var video_urls = [
+	"https://www.youtube.com/watch?v=CB7iTh-G0Wo",
+	"https://www.youtube.com/watch?v=jUJ9As47Yq8",
+	"https://www.youtube.com/watch?v=nwIAgxMv7hI",
+	"https://www.youtube.com/watch?v=kKvEllHwnuo",
+	"https://www.youtube.com/watch?v=f-z1q124A54",
+	"https://www.youtube.com/watch?v=3rK-M97rraQ",
+	"https://www.youtube.com/watch?v=_oE5GRu5oUA",
+	"https://www.youtube.com/watch?v=NhAGq1g2OZs",
+	"https://www.youtube.com/watch?v=zuNSoKsIxJo",
+	"https://www.youtube.com/watch?v=ocTTHkQZsBw",
+	"https://www.youtube.com/watch?v=snpnKcCFFus",
+	"https://www.youtube.com/watch?v=K_xwj9bHZm4",
+	"https://www.youtube.com/watch?v=B7D9YKRomUM"
+];
